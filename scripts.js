d81c3c53602c7aa76e7207a14919026589c9f1ed
siddhantchopra <siddanthchopra@gmail.com>
2020-07-25 23:44:33 +0530

    Updated code

---
+++ scripts.js
@@ -0,0 +1,42 @@
+const draggables = document.querySelectorAll('.draggable')
+const containers = document.querySelectorAll('.container')
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', ()=>{
+       draggable.classList.add('dragging')
+    })
+    draggable.addEventListener('dragend', ()=>{
+        draggable.classList.remove('dragging')
+     })
+})
+
+containers.forEach(container =>{
+    container.addEventListener('dragover',e=>{
+        e.preventDefault()
+        const AfterElement = getDragAfterElement(container, e.clientY)
+        const draggable = document.querySelector('.dragging')
+        if(AfterElement == null){
+            container.appendChild(draggable)
+        } else{
+            container.insertBefore(draggable, AfterElement)
+        }
+    })
+})
+
+function getDragAfterElement(container, y){
+  const draggableElements =  [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+  return draggableElements.reduce((closest, child)=>{
+    const box = child.getBoundingClientRect()
+    const offset = y - box.top - box.height /2
+    if(offset < 0 && offset > closest.offset){
+        return {
+            offset: offset, element: child
+        }
+    }
+        else {
+           return closest
+        }
+    
+  },{offset: Number.NEGATIVE_INFINITY}).element
+}
